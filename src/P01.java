d141607bfa6149feb25b74fab757b2cfae5a7bdb
Berk Babayiğit <berk@gmail.com>
2024-04-27 21:39:02 +0300

    first commit

---
+++ src/P01.java
@@ -0,0 +1,9 @@
+public class P01 {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+
+        int a=120, b=80, c;
+        c=a+b;
+        System.out.println("c="+(a+b));
+    }
+}
