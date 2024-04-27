0b1664418497f8b015600bba91ae9c00fb8980dc
Murat BABAYIGIT <107280767+muratbabayigit@users.noreply.github.com>
2024-04-27 22:19:48 +0300

    Update P02.java

--- src/P02.java
+++ src/P02.java
@@ -4,8 +4,8 @@ public class P02 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.print("Adınızı Giriniz:");
-        String isim= scan.nextLine();
-        System.out.println("Girdiğiniz isim "+isim+" olarak kaydedildi");
+        String name= scan.nextLine();
+        System.out.println("Girdiğiniz isim "+name+" olarak kaydedildi");
 
     }
 }
